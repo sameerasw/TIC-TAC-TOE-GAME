99f683c89305dab3bbd23f446d14847ea39bfff7
sameerasw <sameera.whoami@gmail.com>
2024-07-12 17:37:11 +0530

    implemented 2 player input

--- src/App.js
+++ src/App.js
@@ -13,6 +13,9 @@ export default function Board() {
   const [square, setSquare] = useState(Array(9).fill(null));
 
   function handleClick(i) {
+    if (square[i]) {
+      return;
+    }
     const nextSquare = square.slice();
     if (xIsNext) {
       nextSquare[i] = "X";
@@ -42,4 +45,26 @@ export default function Board() {
       </div>
     </>
   );
+}
+
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2], // Horizontal
+    [3, 4, 5], // Horizontal
+    [6, 7, 8], // Horizontal
+    [0, 3, 6], // Vertical
+    [1, 4, 7], // Vertical
+    [2, 5, 8], // Vertical
+    [0, 4, 8], // Diagonal
+    [2, 4, 6], // Diagonal
+  ];
+  
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+
+  return null;
 }
